4fea9927e7799afc1154166595cc5dd6fa1a767b
Mubarak1A <adesinamubarak123@example.com>
2023-12-27 09:17:46 +0100

    first test successful after opening image file, return an img obj

--- process_image.py
+++ process_image.py
@@ -1,7 +1,10 @@
 #!/bin/pythom3
 """
 Python Script that process image files base on specify request
-
 """
 
-from PIL import image
+from PIL import Image
+
+img = Image.open('./image_sample.jpg')
+
+print(img)
